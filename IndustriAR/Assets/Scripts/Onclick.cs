d1cd39a9eae2eb82cdf368ffe47674f12216cb97
sansotmon <sansotmon@gmail.com>
2020-07-30 22:20:46 -0500

    Add script for on click objects in the escene

---
+++ IndustriAR/Assets/Scripts/Onclick.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Onclick : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                if (hit.transform != null)
+                {
+                    printName(hit.transform.gameObject);
+                }
+            }
+        }
+    }
+
+    private void printName(GameObject go) {
+        print(go.name);
+    }
+}
